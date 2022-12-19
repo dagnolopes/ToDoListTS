6b0955e6c7e48ead06cfc1e02b5d63d41d246951
Dagno Lopes <dagno.lopes2019@gmail.com>
2022-12-19 20:23:09 -0300

    Base projeto

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import style from "./Header.module.css";
+export function Header() {
+  return (
+    <>
+      <div>header</div>
+      <div>header</div>
+      <div>header</div>
+      <div>header</div>
+    </>
+  );
+}
